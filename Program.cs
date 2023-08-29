745f69e45d8a8f6160881e165879a2122f1ae2f1
566325 <nethisireesha288@gmail.com>
2023-08-29 16:31:44 +0530

    [Siri]:EmpWage case Statement

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            int Part_Time= 1;
-            int Full_Time= 2;
-            int Emp_Rate_Per_Hour = 20;
+            const int Part_Time = 1;
+            const int Full_Time = 2;
+            const int Emp_Rate_Per_Hour = 20;
 
             int empHrs = 0;
             int empWage = 0;
@@ -14,20 +14,32 @@
             Random random = new Random();
 
             int cheCheck = random.Next(0,3);
-            if (cheCheck == Part_Time) 
-            {
-                empHrs = 4;
 
-            }
-            else if (cheCheck == Full_Time)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
-            }
+            //if (cheCheck == Part_Time) 
+            //{
+            //    empHrs = 4;
 
+            //}
+            //else if (cheCheck == Full_Time)
+            //{
+            //    empHrs = 8;
+            //}
+            //else
+            //{
+            //    empHrs = 0;
+            //}
+            switch (cheCheck)
+            {
+                case Full_Time:
+                    empHrs = 8;
+                    break;
+                case Part_Time:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
             empWage = empHrs * Emp_Rate_Per_Hour;
             Console.WriteLine("Employee Daily Wage " + empWage);
 
