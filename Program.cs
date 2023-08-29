108230a47020dd5ead31ce874865e79065f05d3a
566325 <nethisireesha288@gmail.com>
2023-08-29 15:36:14 +0530

    [Sirresha] : Added dailywages caluculation

--- Program.cs
+++ Program.cs
@@ -5,19 +5,28 @@
         static void Main(string[] args)
         {
             int Present = 1;
+            int Emp_Rate_Per_Hour = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == Present)
+            int cheCheck = random.Next(0,2);
+            if (cheCheck == Present) 
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
 
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
-
+                empHrs = 0;
             }
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Employee Daily Wage " + empWage);
+
+
+
+
         }
     }
 }
