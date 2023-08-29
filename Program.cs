dacef6f0505e4818c5bb4b59422fa6fb640fbf6c
566325 <nethisireesha288@gmail.com>
2023-08-29 15:59:42 +0530

    [Sireesha]: added PartTime Employee and Wage

--- Program.cs
+++ Program.cs
@@ -4,23 +4,30 @@
     {
         static void Main(string[] args)
         {
-            int Present = 1;
+            int Part_Time= 1;
+            int Full_Time= 2;
             int Emp_Rate_Per_Hour = 20;
 
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
-            int cheCheck = random.Next(0,2);
-            if (cheCheck == Present) 
+
+            int cheCheck = random.Next(0,3);
+            if (cheCheck == Part_Time) 
+            {
+                empHrs = 4;
+
+            }
+            else if (cheCheck == Full_Time)
             {
                 empHrs = 8;
-
             }
             else
             {
                 empHrs = 0;
             }
+
             empWage = empHrs * Emp_Rate_Per_Hour;
             Console.WriteLine("Employee Daily Wage " + empWage);
 
