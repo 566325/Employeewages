ec65247725c5507294dae6695a4be8c3de897f5a
566325 <nethisireesha288@gmail.com>
2023-08-29 14:11:00 +0530

    [Sireesha]: Employee present/absent

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EmployeeWageApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Present = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Present)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+
+            }
+        }
+    }
+}
